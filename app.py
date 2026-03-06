76a32e4b405177639d7271e5b79ce6845be67947
MUGESH71 <mugeshr.ad24@bitsathy.ac.in>
2026-03-06 14:59:51 +0530

    Initial project structure

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Police Case Management System")
+
+
+st.header("Welcome to Dashboard")
+menu=st.sidebar.selectbox(
+    "Menu",
+    [
+        "Register Officer",
+        "View Officers",
+        "Register Case",
+        "Assign Officer to Case",
+        "Search Case",
+        "Update Case Status",
+        "Case Dashboard"
+    ]
+    )
+
